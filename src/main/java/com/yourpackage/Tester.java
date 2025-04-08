ef210b81496db00d04563e6bdc0e5831c9dc86ba
liamkernan <liamkernan0@gmail.com>
2025-04-08 00:01:27 -0400

    first full commit hopefully

--- src/main/java/com/yourpackage/Tester.java
+++ src/main/java/com/yourpackage/Tester.java
@@ -5,7 +5,7 @@ public class Tester {
     public static ArrayList<Tester> list = new ArrayList<Tester>();
     private String prompt;
     private boolean correct = true;
-    private String grah = "";
+
 
     public Tester(String prompt, boolean correct){
         this.correct = correct;
